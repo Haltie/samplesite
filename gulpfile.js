26d5be2dedb058d9c77794b00c47eab45f6625f2
Haltie <halt.develop@gmail.com>
2017-02-26 18:16:03 +0900

    gulpのタスクを修正

--- gulpfile.js
+++ gulpfile.js
@@ -3,27 +3,28 @@ var pug = require('gulp-pug');
 var riot = require('gulp-riot');
 var concat = require('gulp-concat');
 
-var target = ['./app/tags/*.pug', './app/tags/**/*.pug'];
+var layoutTarget = ['app/layout/index.pug'];
+var tagtarget = ['./app/tags/*.pug', './app/tags/**/*.pug'];
 var output = './public/scripts';
 
 gulp.task('pug', function() {
   gulp
-  .src('app/layout/index.pug')
-  .pipe(pug())
-  .pipe(gulp.dest('public/'));
+    .src(layoutTarget)
+    .pipe(pug())
+    .pipe(gulp.dest('public/'));
 });
 
 gulp.task('riot', function() {
   gulp
-    .src(target)
+    .src(tagtarget)
     .pipe(riot({template: 'pug'}))
     .pipe(concat('tags.js'))
-    .pipe(gulp.dest(output))
-    ;
+    .pipe(gulp.dest(output));
 });
 
 gulp.task('watch', function(){
-  gulp.watch(target, ['riot']);
+  gulp.watch(tagtarget, ['riot']);
+  gulp.watch(layoutTarget, ['pug']);
 });
 
-gulp.task('default', ['riot', 'pug']);
+gulp.task('default', ['riot', 'pug', 'watch']);
