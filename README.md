32a912206570e79198153973855e692072033a58
Haltie <halt.develop@gmail.com>
2017-02-26 17:48:21 +0900

    全体の構成を決定

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # samplesite
 
-gulp + riot.js + pug を使ってお試しサイト作るテスト
+gulp + riot.js + pug を使ってお試しサイト作るテスト  
+[かなーり昔に作ったサイト](http://hidamari-finder.com/)(リンク先ドール画像注意)に近い見た目になったら一旦完成  
+アニメーション周りは今回は気にしない  
+
+各tagの構成はこんな感じ(の予定)  
+![](http://study.lugn-design.com/site/samplesite/public/images/readme/site.jpg)
--- public/scripts/tags.js
+++ public/scripts/tags.js
@@ -1,3 +1,15 @@
 
-riot.tag2('app', '<h1> hgoepiyo!!!</h1>', '', '', function(opts) {
+riot.tag2('app-content', '<p>content!</p> <app-single></app-single>', '', '', function(opts) {
+});
+
+riot.tag2('app-main', '<h1> hgoepiyo!!!</h1>', '', '', function(opts) {
+});
+
+riot.tag2('app-menu', '<p>menu!</p>', '', '', function(opts) {
+});
+
+riot.tag2('app-single', '<p>single!</p>', '', '', function(opts) {
+});
+
+riot.tag2('app', '<app-main></app-main> <app-menu></app-menu> <app-content></app-content>', '', '', function(opts) {
 });
